472c5738475f03bd5e513711fb0159470582767d
RAKOTOZAFY Nantenaina Jeremiah <nantenainajeremiah@gmail.com>
2024-08-03 12:50:17 +0300

    adding some bugs hehehe

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -121,7 +121,7 @@ function Admin() {
   };
 
   const handleChangeTrain = (e) => {
-    setFormDataTrain({ ...formDataTrain, [immatriculation]: formDataTrain.immatriculation, [codeItineraire]: formDataTrain.codeItineraire });
+    setFormDataTrain({ ...formDataTrain, [e.target.name]: e.target.value });
   };
 
   const trainSubmit = async (e) => {
@@ -637,6 +637,7 @@ function Admin() {
                               name="immatriculation"
                               value={formDataTrain.immatriculation}
                               id="immatriculation"
+                              autoComplete="immatriculation"
                               className="block w-full border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                               placeholder="Immatriculation du Train"
                               onChange={handleChangeTrain}
@@ -657,6 +658,7 @@ function Admin() {
                             id="itineraire"
                             name="codeItineraire"
                             value={formDataTrain.codeItineraire}
+                            autoComplete="itineraire-name"
                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             onChange={handleChangeTrain}
                           >
